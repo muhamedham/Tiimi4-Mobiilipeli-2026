0d8a6e295e2a8987772b140f02a6310e4aba3e21
Mustafa Hasoon <mustafa.hasoon@tuni.fi>
2026-04-14 13:24:36 +0300

    Käännös sverige

--- Memotap/src/UI/Menus/Translate.cs
+++ Memotap/src/UI/Menus/Translate.cs
@@ -1,26 +1,45 @@
 using Godot;
-
 public partial class Translate : Node
 {
     [Export] Button English;
     [Export] Button Suomi;
-    private string[] languageNames = { "English", "Suomi" };
-    private string[] languageCodes = { "en", "fi" };
+    [Export] Button Svenska;
+
+    private string[] languageNames = { "English", "Suomi", "Svenska" };
+    private string[] languageCodes = { "en", "fi", "sv" };
 
     public override void _Ready()
     {
-        GetNode<Button>("ButtonEnglish").Pressed += OnButtonEnglishPressed;
-        GetNode<Button>("ButtonSuomi").ButtonUp += OnButtonSuomiButtonUp;
+        English.Pressed += OnButtonEnglishPressed;
+        Suomi.Pressed += OnButtonSuomiPressed;
+        Svenska.Pressed += OnButtonSvenskaPressed;
+
+        English.Visible = false;
+        Suomi.Visible = true;
+        Svenska.Visible = false;
     }
 
     private void OnButtonEnglishPressed()
     {
         TranslationServer.SetLocale("en");
-
+        English.Visible = false;
+        Suomi.Visible = true;
+        Svenska.Visible = false;
     }
 
-    private void OnButtonSuomiButtonUp()
+    private void OnButtonSuomiPressed()
     {
         TranslationServer.SetLocale("fi");
+        English.Visible = false;
+        Suomi.Visible = false;
+        Svenska.Visible = true;
+    }
+
+    private void OnButtonSvenskaPressed()
+    {
+        TranslationServer.SetLocale("sv");
+        English.Visible = true;
+        Suomi.Visible = false;
+        Svenska.Visible = false;
     }
 }
