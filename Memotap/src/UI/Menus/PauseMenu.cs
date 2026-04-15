9e51b54a24f0c74ac70667a68620a575e0d827d8
Mustafa Hasoon <mustafa.hasoon@tuni.fi>
2026-04-15 14:48:55 +0300

    Button sizing

--- Memotap/src/UI/Menus/PauseMenu.cs
+++ Memotap/src/UI/Menus/PauseMenu.cs
@@ -2,16 +2,16 @@
 using Godot;
 using System;
 
-public partial class PauseMenu : ColorRect
+public partial class PauseMenu : Control
 {
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		// make sure the pause menu or gameover menu is not visible when first starting the game.
-        Hide();
+		Hide();
 	}
 
-    // Called when PauseButton is pressed
+	// Called when PauseButton is pressed
 	public void OnPausePressed()
 	{
 		Engine.TimeScale = 0;
@@ -26,24 +26,24 @@ public partial class PauseMenu : ColorRect
 		Engine.TimeScale = 1;
 
 		GetTree().Paused = false;
-        Hide();
-    }
+		Hide();
+	}
 
-    // Called when RestartButton is pressed
-    private void OnRestartPressed()
-    {
-		Engine.TimeScale = 1;
-        GetTree().Paused = false;
-        Hide();
-        GetTree().ReloadCurrentScene();
-    }
-
-    // Called when QuitButton is pressed
-    private void OnQuitPressed()
-    {
+	// Called when RestartButton is pressed
+	private void OnRestartPressed()
+	{
 		Engine.TimeScale = 1;
 		GetTree().Paused = false;
 		Hide();
-        GetTree().ChangeSceneToFile("res://scenes/Menu.tscn");
-    }    
+		GetTree().ReloadCurrentScene();
+	}
+
+	// Called when QuitButton is pressed
+	private void OnQuitPressed()
+	{
+		Engine.TimeScale = 1;
+		GetTree().Paused = false;
+		Hide();
+		GetTree().ChangeSceneToFile("res://scenes/Menu.tscn");
+	}
 }
